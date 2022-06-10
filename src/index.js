4afd44e2d366a54ff4fc79b1e55d7f0b9f54409d
Ablaze99 <femiemma99@gmail.com>
2022-06-10 07:17:00 +0100

    react tutorial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from "react";
 import ReactDom from 'react-dom'
-
+import './index.css';
+const title = 'Rotten to The Core and the root'
+     const Author = 'Chris Hemsworth'
+     const img = 'https://m.media-amazon.com/images/I/81j04LrmYDL. _AC_UL320_.jpg'
  function BookList() {
    return (
-    <section>
+    <section className='booklist'>
      <Book />
      </section>
    );
  }
-
  const Book = () => {
-   return <article>This is a Book</article>
- }
- 
+   
+   return (
+     <article className='book'>
+     <img 
+   src= {img}alt=''
+   />
+   <h1>{title}</h1>
+   <h4>{Author}</h4>
+   </article>
+   );
+    };
+
 
 
 ReactDom.render (<BookList />, document.getElementById('root'));
