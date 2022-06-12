f9d62e9cf1750e6a96edebfe815d0321f88cd246
Ablaze99 <femiemma99@gmail.com>
2022-06-13 00:00:29 +0100

    react app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
 import React from "react";
 import ReactDom from 'react-dom'
 import './index.css';
-const title = 'Rotten to The Core and the root'
-     const Author = 'Chris Hemsworth'
-     const img = 'https://m.media-amazon.com/images/I/81j04LrmYDL. _AC_UL320_.jpg'
+
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/81j04LrmYDL. _AC_UL320_.jpg',
+  title:'Rotten to The Core and the root',
+  author: 'Chris Hemsworth'
+}
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/81P9FK0PXyL._AC_UL320_.jpg',
+  title:'The alpha contract',
+  author: 'Elliot Grayson'
+};
+
  function BookList() {
    return (
     <section className='booklist'>
-     <Book />
+     <Book  img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+
+     </Book>
+     
+     <Book  img={secondBook.img} title={secondBook.title} 
+     author={secondBook.author}/>
+    
      </section>
    );
  }
- const Book = () => {
-   
+ const Book = (props) => {
+    const { img, title, author,  } = props;
    return (
      <article className='book'>
      <img 
-   src= {img}alt=''
-   />
+   src= {img}alt=''/>
    <h1>{title}</h1>
-   <h4>{Author}</h4>
+   <h4>{author}</h4>
    </article>
    );
     };
