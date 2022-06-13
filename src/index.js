1151399231fd74fb5a40384bf7d577464eb88974
Ablaze99 <femiemma99@gmail.com>
2022-06-13 01:22:07 +0100

    React Tutorial

--- src/index.js
+++ src/index.js
@@ -2,38 +2,47 @@ import React from "react";
 import ReactDom from 'react-dom'
 import './index.css';
 
-const firstBook = {
+const books = [
+  {  id: 1,
   img: 'https://m.media-amazon.com/images/I/81j04LrmYDL. _AC_UL320_.jpg',
   title:'Rotten to The Core and the root',
-  author: 'Chris Hemsworth'
-}
-const secondBook = {
+  author: 'Chris Hemsworth',
+},
+
+{  
+  id: 2,
   img: 'https://m.media-amazon.com/images/I/81P9FK0PXyL._AC_UL320_.jpg',
   title:'The alpha contract',
-  author: 'Elliot Grayson'
-};
+  author: 'Elliot Grayson',
+},
+{  
+  id: 3,
+  img: 'https://m.media-amazon.com/images/I/81823bTjKHL._AC_UL320_.jpg',
+  title:'The Last Thing He Told Me',
+  author: 'Laura Dave',
+},
+];  
 
  function BookList() {
-   return (
-    <section className='booklist'>
-     <Book  img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-
-     </Book>
-     
-     <Book  img={secondBook.img} title={secondBook.title} 
-     author={secondBook.author}/>
-    
-     </section>
-   );
+   return  <section className='booklist'>{books.map((book, index)=>{
+     const { img, title, author } = book;
+     return <Book key={book.id} {...book}></Book>;
+    })}</section>;
  }
+
  const Book = (props) => {
     const { img, title, author,  } = props;
+    const clickHandler= () => {
+      alert('hello world');
+    }
    return (
      <article className='book'>
      <img 
    src= {img}alt=''/>
    <h1>{title}</h1>
    <h4>{author}</h4>
+   <button type='button' onClick={clickHandler}>ref button</button>
+   
    </article>
    );
     };
